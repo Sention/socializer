37a80abab590d492b9949374b786ff46e0044059
Sention <tdangelov01@gmail.com>
2023-07-12 16:29:26 +0300

    css is now working on server

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'views', 'index.html'));
+    res.sendFile(path.join(__dirname,'/views', 'index.html'));
 });
 
 //Listening to the server
