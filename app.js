52223891f66414fdda7088ca5e1cd666165694ba
Sention <tdangelov@outlook.com>
2023-07-15 17:25:11 +0300

    added passport

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 const path = require('path');
 
 const app = express();
@@ -9,6 +10,12 @@ app.use(express.static(path.join(__dirname, '/views')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'/views', 'index.html'));
 });
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+
+
 
 //Listening to the server
 app.listen(3000, () => console.log('listening on 3000'));
