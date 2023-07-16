e43e87b3e1e06a08820f3726a404ef4de3258b93
Sention <tdangelov@outlook.com>
2023-07-16 14:41:16 +0300

    changed client secret

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const passport = require ('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const GOOGLE_CLIENT_ID = '968640556018-5cl40tmbcv1908n1s7maa67c18c5ueea.apps.googleusercontent.com';
-const GOOGLE_CLIENT_SECRET = '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';
+const GOOGLE_CLIENT_SECRET = 'GGOCSPX-j9PE2jKN3xvb5MEWoLmXnmN5lngL';
 
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
