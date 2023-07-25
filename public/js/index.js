8333452c0892664485841eb20b2f90ad06845830
Sention <tdangelov@outlook.com>
2023-07-25 12:55:07 +0300

    routing if user is logged in

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+/* function checkAndRedirect() {
+    fetch('auth/protected')
+        .then(respone => {
+            if(respone.ok){
+                window.location.href = 'auth/protected';
+            }
+        })
+        .catch(error => {
+            console.error('Error checking  authentication status: ', error)
+        });
+    }
+
+checkAndRedirect(); */
